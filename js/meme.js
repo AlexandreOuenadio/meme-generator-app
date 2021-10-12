63cba1dfb9edf708cd5e51d69996af947151dbbb
Alexandre <alexandre.ouenadio@cpe.fr>
2021-10-12 08:52:09 +0200

    back to github

--- js/meme.js
+++ js/meme.js
@@ -1,7 +1,10 @@
 const canvas = document.getElementById('generator-canvas');
 const ctx = canvas.getContext('2d');
+
 const generate = document.getElementById('generate');
-const imgContainer = document.getElementById('img-container');
+const downloadBtn = document.getElementById("download-btn");
+const memeText = document.getElementById('generator-text');
+
 
 async function generateImg(){
   const response = await fetch('https://api.imgflip.com/get_memes', {mode:"cors"});
@@ -11,17 +14,42 @@ async function generateImg(){
   const imgURL = memes[randomNumber].url;
   
   const img = new Image();
-
+  img.crossOrigin = "anonymous";
   img.src = imgURL;
 
   img.addEventListener('load', () =>{
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    
     canvas.width = img.width;
     canvas.height = img.height;
 
     ctx.drawImage(img, 0,0);
+    memeText.addEventListener('change', ()=> {
+      ctx.clearRect(0,0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0,0);
+      ctx.lineWidth = 10;
+      ctx.lineCap = 'round';
+      ctx.font = '48px sans-serif';
+      ctx.fillStyle = "#fff";
+      ctx.textAlign = 'center';
+      ctx.strokeText(memeText.value,canvas.width/2,canvas.height/2);
+      ctx.fillText(memeText.value,canvas.width/2,canvas.height/2);
+    })
+    
+
   })
   
 }
 
+function downloadMeme(){
+  downloadBtn.download = 'yourMeme.png';
+  console.log(canvas.toDataURL());
+  downloadBtn.href = canvas.toDataURL();
+
+}
+
+
+
 
 generate.addEventListener('click', generateImg);
+downloadBtn.addEventListener('click', downloadMeme);
