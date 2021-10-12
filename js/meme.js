240c3f1e2c665ac420ff05282401aa5971e0e2de
Alexandre <alexandre.ouenadio@cpe.fr>
2021-10-12 10:07:11 +0200

    sliders for text

--- js/meme.js
+++ js/meme.js
@@ -5,6 +5,31 @@ const generate = document.getElementById('generate');
 const downloadBtn = document.getElementById("download-btn");
 const memeText = document.getElementById('generator-text');
 
+const sliderX = document.getElementById('sliderX');
+const sliderY = document.getElementById('sliderY');
+
+const memeTextSize = document.getElementById('meme-text-size');
+
+memeTextSize.min = 24;
+memeTextSize.max = 96;
+
+
+let isMeme = false;
+
+function updateCanvas(img){
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0,0);
+      
+      ctx.lineWidth = 10;
+      ctx.lineCap = 'round';
+      ctx.font = memeTextSize.value + "px sans-serif";
+      ctx.fillStyle = "#fff";
+      ctx.textAlign = 'center';
+      ctx.strokeText(memeText.value, sliderX.value,sliderY.value);
+      ctx.fillText(memeText.value, sliderX.value,sliderY.value);
+      isMeme = true;
+}
+
 
 async function generateImg(){
   const response = await fetch('https://api.imgflip.com/get_memes', {mode:"cors"});
@@ -23,28 +48,48 @@ async function generateImg(){
     canvas.width = img.width;
     canvas.height = img.height;
 
+    sliderX.min = 0;
+    sliderX.max = canvas.width;
+
+    sliderY.min = 0;
+    sliderY.max = canvas.height;
+
+
     ctx.drawImage(img, 0,0);
+
     memeText.addEventListener('change', ()=> {
-      ctx.clearRect(0,0, canvas.width, canvas.height);
-      ctx.drawImage(img, 0,0);
-      ctx.lineWidth = 10;
-      ctx.lineCap = 'round';
-      ctx.font = '48px sans-serif';
-      ctx.fillStyle = "#fff";
-      ctx.textAlign = 'center';
-      ctx.strokeText(memeText.value,canvas.width/2,canvas.height/2);
-      ctx.fillText(memeText.value,canvas.width/2,canvas.height/2);
+      updateCanvas(img);
+
     })
+
+    sliderX.addEventListener('change', ()=> {
+      updateCanvas(img);
+    })
+
+    sliderY.addEventListener('change', ()=> {
+      updateCanvas(img);
+    })
+
+    memeTextSize.addEventListener('change', ()=> {
+      updateCanvas(img);
+    })
+
+    isMeme = true;
     
 
   })
   
 }
 
-function downloadMeme(){
-  downloadBtn.download = 'yourMeme.png';
-  console.log(canvas.toDataURL());
-  downloadBtn.href = canvas.toDataURL();
+function downloadMeme(e){
+  if(isMeme){
+    downloadBtn.download = 'yourMeme.png';
+    downloadBtn.href = canvas.toDataURL();
+  }else{
+    e.preventDefault();
+    console.log("Vous ne pouvez pas télécharger l'image")
+  }
+  
 
 }
 
