2b8f5300a830e6ddc30c80b9cf1302fd743c87a2
Alexandre <alexandre.ouenadio@cpe.fr>
2021-10-12 12:10:29 +0200

    plusieurs text - verison bugguée

--- js/meme.js
+++ js/meme.js
@@ -4,30 +4,76 @@ const ctx = canvas.getContext('2d');
 const generate = document.getElementById('generate');
 const downloadBtn = document.getElementById("download-btn");
 const memeText = document.getElementById('generator-text');
+const textChoice1 = document.getElementById('text1');
+const textChoice2 = document.getElementById('text2');
+const textChoice3 = document.getElementById('text3');
+const textChoice4 = document.getElementById('text4');
 
 const sliderX = document.getElementById('sliderX');
 const sliderY = document.getElementById('sliderY');
-
 const memeTextSize = document.getElementById('meme-text-size');
 
 memeTextSize.min = 24;
 memeTextSize.max = 96;
 
-
+let activeText = 0;
 let isMeme = false;
+
+
+const texts = [
+  {
+    text: " ",
+    fontSize: 24,    //Texte 1
+    x: 0,
+    y: 0
+  },
+
+  {
+    text: " ",
+    fontSize: 24,   //Texte 2
+    x: 0,
+    y: 0
+  },
+  {
+    text: " ",
+    fontSize: 24,   //Texte 3
+    x: 0,
+    y: 0,
+  },
+  {
+    text: " ",
+    fontSize: 24,   //Texte 4
+    x: 0,
+    y: 0,
+  }
+];
+
+
+
+
+
+
+
 
 function updateCanvas(img){
   ctx.clearRect(0,0, canvas.width, canvas.height);
-      ctx.drawImage(img, 0,0);
-      
-      ctx.lineWidth = 10;
-      ctx.lineCap = 'round';
-      ctx.font = memeTextSize.value + "px sans-serif";
-      ctx.fillStyle = "#fff";
-      ctx.textAlign = 'center';
-      ctx.strokeText(memeText.value, sliderX.value,sliderY.value);
-      ctx.fillText(memeText.value, sliderX.value,sliderY.value);
-      isMeme = true;
+  ctx.drawImage(img, 0,0);
+  ctx.lineWidth = 10;
+  ctx.lineCap = 'round';
+  ctx.fillStyle = "#fff";
+  ctx.textAlign = 'center';
+  ctx.font = memeTextSize.value + "px sans-serif";
+
+
+  for (let i=0; i < texts.length; i++){
+    ctx.strokeText(texts[i].text, texts[i].x,texts[i].y);
+    ctx.fillText(texts[i].text, texts[i].x,texts[i].y);
+  }
+
+  ctx.strokeText(memeText.value, sliderX.value,sliderY.value);
+  ctx.fillText(memeText.value, sliderX.value,sliderY.value);
+
+  isMeme = true;
 }
 
 
@@ -56,6 +102,32 @@ async function generateImg(){
 
 
     ctx.drawImage(img, 0,0);
+
+    textChoice1.addEventListener('click', ()=>{
+      saveText(memeText.value,sliderX.value,sliderY.value);
+      activeText = 0;
+      updateCanvas(img);
+      
+    })
+    
+    textChoice2.addEventListener('click', ()=>{
+      saveText(memeText.value,sliderX.value,sliderY.value);
+      activeText = 1;
+      updateCanvas(img);
+      
+    })
+    
+    textChoice3.addEventListener('click', ()=>{
+      saveText(memeText.value,sliderX.value,sliderY.value);
+      activeText = 2;
+      updateCanvas(img);
+    })
+    
+    textChoice4.addEventListener('click', ()=>{
+      saveText(memeText.value,sliderX.value,sliderY.value);
+      activeText = 3;
+      updateCanvas(img);
+    })
 
     memeText.addEventListener('change', ()=> {
       updateCanvas(img);
@@ -93,8 +165,20 @@ function downloadMeme(e){
 
 }
 
+function saveText(text, x, y,size){
+
+    texts[activeText].text = text;
+    texts[activeText].x = x;
+    texts[activeText].y = y;
+    texts[activeText].fontSize = size;
+
+
+
+}
+
 
 
 
 generate.addEventListener('click', generateImg);
 downloadBtn.addEventListener('click', downloadMeme);
+
