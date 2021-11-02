1339617de7a6d4dfe38f2d9e0229904d52cf6762
Zaph <zmichalop@gmail.com>
2021-11-02 17:27:27 +0100

    id products

--- js/boutique.js
+++ js/boutique.js
@@ -9,10 +9,12 @@ function cloner(prod){
     let img=clone.querySelector("img");
     let title=clone.querySelector("h1");
     let price=clone.querySelector("h2");
+    let id=clone.querySelector("a");
     let res = decodeURI(img.src);
     img.src = res.replace(/{{image-Product}}/g, prod["image"]);
     title.textContent=title.textContent.replace(/{{name-Product}}/g,prod["name"]);
     price.textContent=price.textContent.replace(/{{price-Product}}/g,prod["price"]);
+    id.href=id.href.replace(/{{id-Product}}/g,prod["id"]);
     grid.appendChild(clone);
 }
 
