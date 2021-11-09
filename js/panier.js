fa12ff262e8070e1f389f9240164f8afc30f661f
Zaphirios Michalopoulos <z.michalopoulos@etudiant.cpe.fr>
2021-11-09 13:32:59 +0100

    final commit

--- js/panier.js
+++ js/panier.js
@@ -50,39 +50,30 @@ document.getElementById('tax').textContent = tax + "€";
 document.getElementById('total').textContent = total + "€";
 
 function CalculDistance(){
-    let CountryInput= document.getElementById('country').innerHTML;
-    let PostalCodeInput= document.getElementById('postalcode').innerHTML;
-    let ANumberInput= document.getElementById('adressnumber').innerHTML;
-    let ANameInput= document.getElementById('adressname').innerHTML;
+    let CountryInput= document.getElementById('country').value;
+    let PostalCodeInput= document.getElementById('postalcode').value;
+    let ANumberInput= document.getElementById('adressnumber').value;
+    let ANameInput= document.getElementById('adressname').value;
     let Coordinates;
-    FetchCoordinates(CountryInput+PostalCodeInput+ANumberInput+ANameInput+".json");
-    console.log(Coordinates);
-    let distance=FetchDistance(Coordinates);
-    
-}
-
-
-function FetchCoordinates(Adress){
-    fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/3 Rue Victor Grignard 69100 Villeurbanne.json?access_token=pk.eyJ1IjoiZWxpdGVuZXQxMCIsImEiOiJja3ZyNWd6M2c2a2M1MnJvazY2andqbTJoIn0.8lUMtWhONmBdIfm7IaiKyg")
+    fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/"+CountryInput+PostalCodeInput+ANumberInput+ANameInput+".json?access_token=pk.eyJ1IjoiZWxpdGVuZXQxMCIsImEiOiJja3ZyNWd6M2c2a2M1MnJvazY2andqbTJoIn0.8lUMtWhONmBdIfm7IaiKyg")
     .then(function(response){
         return response.json();
     })
     .then(function(json){
         Coordinates=json.features[0].center;
-        
+        console.log(Coordinates);
+        let MapboxUrl="https://api.mapbox.com/directions/v5/mapbox/driving/"+Coordinates[0]+","+Coordinates[1]+";45.783850,4.869060?geometries=geojson&access_token=pk.eyJ1IjoiZWxpdGVuZXQxMCIsImEiOiJja3ZyNWVhYW4wNGxoMm9tOW94bjhkaTlxIn0.ldlny4HqeBlMz0Om677T5g";
+        fetch(MapboxUrl)
+            .then(reponse =>{
+                return reponse.json();
+            })
+            .then(distance =>{
+                console.log(distance.waypoints[0].distance);
+            })
+
     })
     
 }
 
 
 
-function FetchDistance(Coordinates){
-    let MapboxUrl="https://api.mapbox.com/directions/v5/mapbox/driving/"+Coordinates[0]+","+Coordinates[1]+"?geometries=geojson&access_token=pk.eyJ1IjoiZWxpdGVuZXQxMCIsImEiOiJja3ZyNWVhYW4wNGxoMm9tOW94bjhkaTlxIn0.ldlny4HqeBlMz0Om677T5g";
-    fetch(MapboxUrl)
-    .then(function(response){
-           return response.json
-    })
-    .then(function(json){
-        
-    })
-}
